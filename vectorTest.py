36074458da88f65cf1bd807ade7246cdfe9b017a
GregWhite2001 <greggregory95@live.com>
2022-06-17 20:28:52 -0500

    new print statements to show integration and differentiation

--- vectorTest.py
+++ vectorTest.py
@@ -2,7 +2,6 @@ from vector import Vector
 import sympy as sym
 
 ##################### MAIN ######################
-sym.init_printing(use_unicode=True)
 t = sym.symbols('t')
 
 print("------------------Constant Vectors---------------")
@@ -25,32 +24,35 @@ vect6 = Vector.crossProduct(vect4,vect2)
 
 print(vect6)
 print(Vector.magnitude(vect6))
-print(f"The vectors and vect4 and vect5 are parallel. ({str(Vector.isParallel(vect4,vect5))})")
+print(Vector.isParallel(vect4,vect5))
 
-print(f"The magnitude of vect6 is: {str(Vector.magnitude(vect6))}")
+print(Vector.magnitude(vect6))
 #print(Vector.crossProductMagnitude(vect4,vect3))
-print(f"vect2 = {str(vect2)}")
-print(f"The magnitude of vect2 is: {str(Vector.magnitude(vect2))}")
+print(vect2)
+print(Vector.magnitude(vect2))
 
 print("------------------Vector Functions---------------")
 x=sym.Function('x')
 y=sym.Function('y')
 z=sym.Function('z')
 r1 = Vector(x(t),y(t),z(t))
-print(f"r(t) = {str(r1)}")
+print(r1)
 r2 = Vector(sym.ln(t), sym.exp(t), sym.tan(t))
 r3 = Vector(2*t**2,1,0)
-print(f"r(t) = {str(r2)}")
-print(f"r(t) = {str(r3)}")
+print(r2)
+print(r3)
 
 print("------------------Differentiating Vector Functions---------------")
-dr = Vector.differentiate(r3)
-print(dr)
-print(Vector.differentiate(Vector(sym.cos(t), sym.exp(t), t)))
-print(Vector.differentiate(Vector(t*sym.cos(t), sym.exp(t)/t, t)))
+dr = Vector.differentiate(r2)
+print("r'(t) =" + str(dr))
+print("r'(t) = "  + str(Vector.differentiate(Vector(sym.cos(t), sym.exp(t), t))))
+print("r'(t) = " + str(Vector.differentiate(Vector(t*sym.cos(t), sym.exp(t)/t, t))))
 
 print("------------------Integrating Vector Functions---------------")
-print(Vector.integrate(Vector(t, 1, 3*t**2)))
+print("R(t) = " + str(Vector.integrate(Vector(t, 1, 3*t**2))))
 print(Vector.magnitude(dr))
 print(Vector.arcLength(dr, 0, 1))
+#print('\u222B')
 #print(type(vect2.z))
+#s= sym.Integral(Vector.magnitude(dr))
+#s
