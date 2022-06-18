b3eaa2d7ec00adaf2be470f57e55e8a1867a55b4
GregWhite2001 <greggregory95@live.com>
2022-06-17 20:35:43 -0500

    same as earlier...

--- vectorTest.py
+++ vectorTest.py
@@ -2,6 +2,7 @@ from vector import Vector
 import sympy as sym
 
 ##################### MAIN ######################
+sym.init_printing(use_unicode=True)
 t = sym.symbols('t')
 
 print("------------------Constant Vectors---------------")
@@ -24,26 +25,26 @@ vect6 = Vector.crossProduct(vect4,vect2)
 
 print(vect6)
 print(Vector.magnitude(vect6))
-print(Vector.isParallel(vect4,vect5))
+print(f"The vectors and vect4 and vect5 are parallel. ({str(Vector.isParallel(vect4,vect5))})")
 
-print(Vector.magnitude(vect6))
+print(f"The magnitude of vect6 is: {str(Vector.magnitude(vect6))}")
 #print(Vector.crossProductMagnitude(vect4,vect3))
-print(vect2)
-print(Vector.magnitude(vect2))
+print(f"vect2 = {str(vect2)}")
+print(f"The magnitude of vect2 is: {str(Vector.magnitude(vect2))}")
 
 print("------------------Vector Functions---------------")
 x=sym.Function('x')
 y=sym.Function('y')
 z=sym.Function('z')
 r1 = Vector(x(t),y(t),z(t))
-print(r1)
+print(f"r(t) = {str(r1)}")
 r2 = Vector(sym.ln(t), sym.exp(t), sym.tan(t))
 r3 = Vector(2*t**2,1,0)
-print(r2)
-print(r3)
+print(f"r(t) = {str(r2)}")
+print(f"r(t) = {str(r3)}")
 
 print("------------------Differentiating Vector Functions---------------")
-dr = Vector.differentiate(r2)
+dr = Vector.differentiate(r3)
 print("r'(t) =" + str(dr))
 print("r'(t) = "  + str(Vector.differentiate(Vector(sym.cos(t), sym.exp(t), t))))
 print("r'(t) = " + str(Vector.differentiate(Vector(t*sym.cos(t), sym.exp(t)/t, t))))
@@ -52,7 +53,4 @@ print("------------------Integrating Vector Functions---------------")
 print("R(t) = " + str(Vector.integrate(Vector(t, 1, 3*t**2))))
 print(Vector.magnitude(dr))
 print(Vector.arcLength(dr, 0, 1))
-#print('\u222B')
 #print(type(vect2.z))
-#s= sym.Integral(Vector.magnitude(dr))
-#s
